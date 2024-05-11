1987d560f042254fb10ae963bd6c3668dcf22f18
unknown <nikita.phulkar@gmail.com>
2024-05-11 19:03:55 +0530

    changes the minutes format

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -29,7 +29,7 @@ function App() {
   const formatTime = (time) => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${minutes.toString().padStart(1, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
   return (
